2acfed39e7fcf470068b566855553d26994d95c9
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-12-20 10:17:45 +0900

    feat: use DataBindingUtil

--- app/src/main/java/com/example/databindingsample/MainActivity.kt
+++ app/src/main/java/com/example/databindingsample/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.databindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.databindingsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var users: MutableList<User> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        users.add(User("aaaaaaa"))
+        users.add(User("bbbbbbb"))
+        users.add(User("ccccccc"))
+        users.add(User("ddddddd"))
+        users.add(User("eeeeeee"))
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
